0db941552e5cb111fe85e3503c83928642ac3ce0
nicole778h <116587664+nicole778h@users.noreply.github.com>
2024-03-23 16:07:34 +0200

    Add files via upload

---
+++ ASC/lab1/adunarecs.asm
@@ -0,0 +1,44 @@
+bits 32 ; assembling for the 32 bits architecture
+
+; declare the EntryPoint (a label defining the very first instruction of the program)
+global start        
+
+; declare external functions needed by our program
+extern exit               ; tell nasm that exit exists even if we won't be defining it
+import exit msvcrt.dll    ; exit is a function that ends the calling process. It is defined in msvcrt.dll
+                          ; msvcrt.dll contains exit, printf and all the other important C-runtime specific functions
+
+; our data is declared here (the variables needed by our program)
+segment data use32 class=data
+    ; ...
+    a db 10
+    b dw 15
+    c dd 20
+    d dq 30
+
+; our code starts here
+segment code use32 class=code
+    start:
+        ; ...
+        mov ax,[c]; ax=20
+        cdq
+        mov bx,[d]; bx=30
+        add ax,bx; ax=20+30=50
+        mov cl,[a];cl=10
+        cbw
+        cdq
+        sub ax,cx; ax=50-10=40
+        mov bx,ax ;bx=40
+        mov ax,[c];ax=20
+        cdq
+        mov cx,[d];cx=30
+        sub cx,ax ;cx=30-20=10
+        sub bx,cx;bx=40-10=30
+        mov ax,[b];ax=15
+        cwd
+        cdq
+        sub bx,ax;bx=30-15=15
+    
+        ; exit(0)
+        push    dword 0      ; push the parameter for exit onto the stack
+        call    [exit]       ; call exit to terminate the program
---
+++ ASC/lab1/inmultirefs.asm
@@ -0,0 +1,50 @@
+bits 32 ; assembling for the 32 bits architecture
+
+; declare the EntryPoint (a label defining the very first instruction of the program)
+global start        
+
+; declare external functions needed by our program
+extern exit               ; tell nasm that exit exists even if we won't be defining it
+import exit msvcrt.dll    ; exit is a function that ends the calling process. It is defined in msvcrt.dll
+                          ; msvcrt.dll contains exit, printf and all the other important C-runtime specific functions
+
+; our data is declared here (the variables needed by our program)
+segment data use32 class=data
+    ; ...
+     a db 5
+    b dd 15
+    c db 25
+    x dq 10
+
+; our code starts here
+segment code use32 class=code
+    start:
+        ; ...
+        movzx eax,byte[a]; eax=a=5
+        mul byte[a]; eax=25
+        mov ebx,[b]; ebx=b=15
+        add eax,ebx; eax=25+15=40
+        mov edx,0; edx:eax=40
+        
+        ;movzx eax,byte[c]; eax=c=25
+        mov bl,[c];al=25
+        mov bh,0; cbw
+        div byte[a]; al=c/a=25/5=5
+        add bx,[a]; ax=10
+        ;mov bx,ax; bx=10
+        movzx eax,ax; cwde
+        mov ecx,eax; edx=10
+        mov eax, ebx; eax=40
+        ;mov ecx,edx;ecx=10
+        div ecx
+        ;mov ecx,eax
+        ;div dx
+        ; movzx eax,bx cwde
+        
+        
+        
+        
+    
+        ; exit(0)
+        push    dword 0      ; push the parameter for exit onto the stack
+        call    [exit]       ; call exit to terminate the program
